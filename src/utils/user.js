6a1e334e89765f8c0083470c049aa8adc601583a
Abdul Hafeez <hafizabdulhafeez240302@gmail.com>
2024-01-28 12:00:24 +0530

    initial commit

---
+++ src/utils/user.js
@@ -0,0 +1,57 @@
+const users = [];
+
+//addUser, removeUser, getUser, getUsersInRoom
+
+const addUser = ({id, username, room}) => {
+    username = username.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+    if(!username || !room) {
+        return {
+            error: 'Username and Room are Required!'
+        }
+    }
+
+    //checking for existing User
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username
+    })
+
+    if(existingUser) {
+        return {
+            error: 'Username is in use!'
+        }
+    }
+
+    //store user
+    const user = {id, username, room} 
+    users.push(user)
+    return{user}
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => user.id === id);
+
+    if(index !== -1) {
+        return users.splice(index, 1)[0];   //will return the id of removed user
+    }
+}
+
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
+
+const getUserInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter((user) => user.room === room)
+}
+
+
+module.exports = {
+    addUser,
+    getUser,
+    getUserInRoom,
+    removeUser
+}
+
+
